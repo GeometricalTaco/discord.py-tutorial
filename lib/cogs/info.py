3895c080a2c30f420ae18000bdc9d8d9100fc586
GeometricalTaco <92734466+GeometricalTaco@users.noreply.github.com>
2021-11-11 12:56:36 +0000

    Added guild info

--- lib/cogs/info.py
+++ lib/cogs/info.py
@@ -17,12 +17,12 @@ class Info(Cog):
 					  colour=target.colour,
 					  timestamp=datetime.utcnow())
 
-		fields = [("ID", target.id, False),
-				  ("Name", str(target), True),
+		fields = [("Name", str(target), True),
+				  ("ID", target.id, True),
 				  ("Bot?", target.bot, True),
 				  ("Top role", target.top_role.mention, True),
 				  ("Status", str(target.status).title(), True),
-				  #("Activity", f"{str(target.activity.type).split('.')[-1].title()} {target.activity.name}", True),
+				  ("Activity", f"{str(target.activity.type).split('.')[-1].title() if target.activity else 'N/A'} {target.activity.name if target.activity else ''}", True),
 				  ("Created at", target.created_at.strftime("%d/%m/%Y %H:%M:%S"), True),
 				  ("Joined at", target.joined_at.strftime("%d/%m/%Y %H:%M:%S"), True),
 				  ("Boosted", bool(target.premium_since), True)]
@@ -34,10 +34,41 @@ class Info(Cog):
 
 		await ctx.send(embed=embed)
 
+
 	@command(name="serverinfo", aliases=["guildinfo", "si", "gi"])
 	async def server_info(self, ctx):
-		pass
+
+		embed = Embed(title="Server Information",
+					  colour=ctx.guild.owner.colour,
+					  timestamp=datetime.utcnow())
+
+		statuses = [len(list(filter(lambda m: str(m.status) == "online", ctx.guild.members))),
+					len(list(filter(lambda m: str(m.status) == "idle", ctx.guild.members))),
+					len(list(filter(lambda m: str(m.status) == "dnd", ctx.guild.members))),
+					len(list(filter(lambda m: str(m.status) == "offline", ctx.guild.members)))]
+
+		fields = [("ID", ctx.guild.id, True),
+				  ("Owner", ctx.guild.owner, True),
+				  ("Region", ctx.guild.region, True),
+				  ("Created at", ctx.guild.created_at.strftime("%d/%m/%Y %H:%M:%S"), True),
+				  ("Members", len(ctx.guild.members), True),
+				  ("Humans", len(list(filter(lambda m: not m.bot, ctx.guild.members))), True),
+				  ("Bots", len(list(filter(lambda m: m.bot, ctx.guild.members))), True),
+				  ("Banned members", len(await ctx.guild.bans()), True),
+				  ("Statuses", f"🟢 {statuses[0]} 🟡 {statuses[1]} 🔴 {statuses[2]} ⚫ {statuses[3]}", True),
+				  ("Text Channels", len(ctx.guild.text_channels), True),
+				  ("Voice Channels", len(ctx.guild.voice_channels), True),
+				  ("Categories", len(ctx.guild.categories), True),
+				  ("Roles", len(ctx.guild.roles), True),
+				  ("Invites", len(await ctx.guild.invites()), True),
+				  ("\u200b", "\u200b", True)]
+
+		for name, value, inline in fields:
+			embed.add_field(name=name, value=value, inline=inline)
+
 		embed.set_thumbnail(url=ctx.guild.icon_url)
+
+		await ctx.send(embed=embed)
 
 	@Cog.listener()
 	async def on_ready(self):
